58711b4fcc6fe55c80864d6f4ea6ff4f202a8a8f
Josefin Hao <ruihao@Ruis-MacBook-Pro.local>
2023-09-20 16:01:37 +0200

    adding 4 users to 30 test groups

--- mass_add.py
+++ mass_add.py
@@ -28,12 +28,28 @@ chat_groups = [
     4080579443,
     4016827893,
     4011107989,
-    4045586431
+    4045586431,
+    4052158893,
+    4006339985,
+    4068448859,
+    4079528775,
+    4014051019,
+    4069259109,
+    4089323929,
+    4076145680,
+    4038409950,
+    4056712866
 ]
 # chat_id = 4023732695
-# user_to_add = 464929491  # Max's TG ID 464929491
+# user_to_add = 464929491  # Max's TG ID
 # user_to_add = 1299245979 # Michael's TG ID
-user_to_add = 5498034564 # Nabeel's TG ID
+# user_to_add = 5498034564 # Nabeel's TG ID
+# user_to_add = 732931592 # Daisy's TG ID
+
+users_to_add = [464929491,
+                1299245979,
+                5498034564,
+                732931592]
 
 # print(client.get_me().stringify())
 
@@ -52,14 +68,15 @@ user_to_add = 5498034564 # Nabeel's TG ID
 
 
 for chat_id in chat_groups:
-    try:
-        client(AddChatUserRequest(
-                chat_id,
-                user_to_add,
-                fwd_limit=100  # Allow the user to see the 100 last messages
-            ))        
-    except Exception as error:
-        print(error) 
+    for user_to_add in users_to_add:
+        try:
+            client(AddChatUserRequest(
+                    chat_id,
+                    user_to_add,
+                    fwd_limit=100  # Allow the user to see the 100 last messages
+                ))        
+        except Exception as error:
+            print(error) 
 
 
 
