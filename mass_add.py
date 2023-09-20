71c952c2661c73e8264877a24535dc785bda87ea
Josefin Hao <ruihao@Ruis-MacBook-Pro.local>
2023-09-20 15:28:19 +0200

    adding to 20 test groups

--- mass_add.py
+++ mass_add.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events, sync, utils
 from telethon.tl.functions.messages import AddChatUserRequest
+import td
  
 api_id = 28660903
 api_hash = '2e6d3d05025f5bd74427d140a45bbc47'
@@ -7,8 +8,32 @@ api_hash = '2e6d3d05025f5bd74427d140a45bbc47'
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
 
-chat_id = 4094437222
-user_to_add = 464929491
+chat_groups = [
+    4023732695,
+    4044874801,
+    4016892206,
+    4035140007,
+    4005957837,
+    4077575650,
+    4062238263,
+    4061582253,
+    4071929846,
+    4062455097,
+    4021989027,
+    4066852879,
+    4093702766,
+    4026493485,
+    4018270475,
+    4015709536,
+    4080579443,
+    4016827893,
+    4011107989,
+    4045586431
+]
+# chat_id = 4023732695
+# user_to_add = 464929491  # Max's TG ID 464929491
+# user_to_add = 1299245979 # Michael's TG ID
+user_to_add = 5498034564 # Nabeel's TG ID
 
 # print(client.get_me().stringify())
 
@@ -25,9 +50,16 @@ user_to_add = 464929491
 #         fwd_limit=10  # Allow the user to see the 10 last messages
 #     ))
 
-client(AddChatUserRequest(
-        chat_id,
-        user_to_add,
-        fwd_limit=100  # Allow the user to see the 100 last messages
-    ))
+
+for chat_id in chat_groups:
+    try:
+        client(AddChatUserRequest(
+                chat_id,
+                user_to_add,
+                fwd_limit=100  # Allow the user to see the 100 last messages
+            ))        
+    except Exception as error:
+        print(error) 
+
+
 
