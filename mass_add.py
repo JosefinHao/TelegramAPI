b2f74a6f1c6a7ed4e2584a18fea003d6f24c0abe
Josefin Hao <ruihao@Ruis-MacBook-Pro.local>
2023-09-22 15:07:26 +0200

    create test groups and get their chat_ids, add users to test groups

--- mass_add.py
+++ mass_add.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events, sync, utils
-from telethon.tl.functions.messages import AddChatUserRequest
+from telethon.tl.functions.messages import AddChatUserRequest, CreateChatRequest
+import telethon.tl.functions
 import re
 
 
@@ -27,7 +28,7 @@ chat_groups = [
     4061582253,
     4071929846,
     4062455097,
-    4021989027,
+    4021989027,  # TestGroup11
     4066852879,
     4093702766,
     4026493485,
@@ -37,7 +38,7 @@ chat_groups = [
     4016827893,
     4011107989,
     4045586431,
-    4052158893,
+    4052158893,  # TestGroup21
     4006339985,
     4068448859,
     4079528775,
@@ -47,6 +48,26 @@ chat_groups = [
     4076145680,
     4038409950,
     4056712866,
+    4045512594,  # TestGroup31
+    4076187874,
+    4021369548,
+    4072186898,
+    4062593072,
+    4000704092,
+    4047866905,
+    4064546411,
+    4052360104,
+    4050278583,
+    4039499664,  # TestGroup41
+    4076996673,
+    4020042822,
+    4043304955,
+    4071065766,
+    4055463727,
+    4031084976,
+    4022473918,
+    4058451998,
+    4075810296,
 ]
 
 users_to_add = [
@@ -62,7 +83,22 @@ api_hash = "2e6d3d05025f5bd74427d140a45bbc47"
 client = TelegramClient("session_name", api_id, api_hash)
 client.start()
 
-for chat_id in chat_groups:
+# create test groups and add users to them all at once, target 600+ groups
+for n in range(30):
+    lincoln_id = 1608449357
+    group_number = 55 + n
+    group_name = "TestGroup" + str(group_number)
+
+    # create a test group and get chat_id
+    new_chat = CreateChatRequest(
+            [lincoln_id],
+            group_name
+    )
+    info = client(new_chat)
+    info_dict = info.to_dict()
+    chat_id = info_dict["updates"][1]["participants"]["chat_id"]
+
+    # add users to the newly created test group
     for user_to_add in users_to_add:
         try:
             client(
